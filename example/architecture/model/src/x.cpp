339489e98877f43b3c33a7a70f2eed0c4759546f
ichiroprogrammer <160194260+ichiroprogrammer@users.noreply.github.com>
2025-11-18 09:09:32 +0900

    delete unnecessary sample

--- example/architecture/model/src/x.cpp
+++
@@ -1,9 +0,0 @@
-#include <chrono>
-#include <functional>
-#include <iostream>
-#include <thread>
-
-#include "./x.h"
-
-X::X() {}
-X::~X() {}
--- example/architecture/model/src/x.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-class X {
-public:
-    X();
-    // something
-    ~X();
-};
